1f240ee28a21bc9edee27611d21f8a53ead9de16
davidmerrick <david@david-merrick.com>
2012-05-31 15:53:54 -0700

    Update rover/tripod/TripodController.c

--- rover/tripod/TripodController.c
+++ rover/tripod/TripodController.c
@@ -31,7 +31,7 @@ void TripodControllerTick(Rover * rov) {
 	commPkt.data=data;
 	char ret;
 	
-	if (CommRXPacketsAvailable(&tripodController.inf) && !TransmissionInProgress(rov)) { //CommRXPacketsAvailable is from /Common/CommInterface/CommInterface.c. 
+	if (CommRXPacketsAvailable(&tripodController.inf) && !TransmissionInProgress(rov)) { //CommRXPacketsAvailable is from /Common/CommInterface/CommInterface.c. TransmissionInProgress() is from /rover/ProcessManager/ProcessManager.c.
 		ret = CommGetPacket(&tripodController.inf, &commPkt, 20); //from /Common/CommInterface/CommInterface.c
 		
 		if (!ret) {
