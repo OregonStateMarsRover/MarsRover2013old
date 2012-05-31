89c9c60900d81a36c3d21875316216c58fdf65ff
davidmerrick <david@david-merrick.com>
2012-05-31 15:40:13 -0700

    Update rover/tripod/TripodController.c

--- rover/tripod/TripodController.c
+++ rover/tripod/TripodController.c
@@ -5,10 +5,10 @@
 #include "../ProcessManager/ProcessManager.h"
 
 void TripodControllerInit() {
-	USART_Open(&tripodController.port, 1, USART_BAUD_38400, 10, 10, true, true); //from Common/Interfaces/Serial/X86/USART.c. Opens up a USART serial to send packets over. Arguments are USART * serial, unsigned char port, unsigned char baud_rate, unsigned short tx_buf, unsigned short rx_buf, bool use_rs485.
-	CommInterfaceInit(&tripodController.inf, &tripodController.port);
+	USART_Open(&tripodController.port, 1, USART_BAUD_38400, 10, 10, true, true); //from /Common/Interfaces/Serial/X86/USART.c. Opens up a USART serial to send packets over. Arguments are USART * serial, unsigned char port, unsigned char baud_rate, unsigned short tx_buf, unsigned short rx_buf, bool use_rs485.
+	CommInterfaceInit(&tripodController.inf, &tripodController.port); //from /Common/CommInterface/CommInterface.c.
 	
-	PacketQueueInit2(&tripodController.pktQueue, 6, 20, tripodController.queuedPackets, tripodController.queuedData);
+	PacketQueueInit2(&tripodController.pktQueue, 6, 20, tripodController.queuedPackets, tripodController.queuedData); //from /rover/PacketQueuer.c
 }
 
 void TripodControllerHandleMessage(Rover * rov, CommPacket * pkt) {
@@ -21,17 +21,17 @@ void TripodControllerHandleMessage(Rover * rov, CommPacket * pkt) {
 	pkt->length=1;
 	SendMessage(rov, pkt);*/
 	
-	PacketQueueAddPacket(&tripodController.pktQueue, pkt);
+	PacketQueueAddPacket(&tripodController.pktQueue, pkt); //from /rover/PacketQueuer.c.
 }
 
 void TripodControllerTick(Rover * rov) {
 	// maybe check in here if we've received data from the accelerometer or barometer
-	CommPacket commPkt; //Struct CommPacket has 
+	CommPacket commPkt; //Struct CommPacket from /Common/CommInterface/CommInterface.h
 	unsigned char data[20];
 	commPkt.data=data;
 	char ret;
 	
-	if (CommRXPacketsAvailable(&tripodController.inf) && !TransmissionInProgress(rov)) {
+	if (CommRXPacketsAvailable(&tripodController.inf) && !TransmissionInProgress(rov)) { 
 		ret = CommGetPacket(&tripodController.inf, &commPkt, 20);
 		
 		if (!ret) {
@@ -44,9 +44,9 @@ void TripodControllerTick(Rover * rov) {
 	}
 	
 	if (PacketQueueCount(&tripodController.pktQueue) && !CommTransmissionInProgress(&tripodController.inf)) { // a queued packet is ready to be transmitted out
-		if (PacketQueueGetPacket(&tripodController.pktQueue, &commPkt)) {
+		if (PacketQueueGetPacket(&tripodController.pktQueue, &commPkt)) { //from /Common/CommInterface/CommInterface.c
 			commPkt.target = 0x02; // target the tripod board
-			CommSendPacket(&tripodController.inf, &commPkt);
+			CommSendPacket(&tripodController.inf, &commPkt); //from /Common/CommInterface/CommInterface.c
 		}
 	}
 }
